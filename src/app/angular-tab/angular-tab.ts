e46518b6d3b03943b2e1ed32d2b8b7cc6c9c8e0c
jeremy-denis <jeremy.denis9@hotmail.fr>
2018-01-27 17:31:59 +0100

    add dynamic tab

--- src/app/angular-tab/angular-tab.ts
+++ src/app/angular-tab/angular-tab.ts
@@ -10,12 +10,18 @@ import { Events } from 'ionic-angular';
 export class AngularTab implements OnInit {
 	@Input() tabs:any;
 	@Input() max:any;
+	other;
 	
 	launchEvent($data,event) {
 		this.events.publish(event, $data);
 	}
-		
+	
+	otherTab() {
+		this.other = !this.other;
+	}
+	
 	ngOnInit(): void {
+		this.other = false;
     }
     
     constructor(public events: Events) {
